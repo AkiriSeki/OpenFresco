e01701932f1a73653c43f32af17d2afac9c1e25f
AkiriSeki <sekia@stanford.edu>
2023-05-02 16:23:02 -0700

    Added cpp files for TCP&UDP. Also, tcp_socket.c and udp_socket.c was copied from c folder and pasted in simulink folder

---
+++ SRC/simApplicationClient/simulink/cpp/create_SFunction.m
@@ -0,0 +1,4 @@
+mex -O SFun_UDPSocketRecv.cpp udp_socket.cpp ws2_32.lib
+mex -O SFun_UDPSocketSend.cpp udp_socket.cpp ws2_32.lib
+mex -O SFun_TCPSocketRecv.cpp tcp_socket.cpp ws2_32.lib
+mex -O SFun_TCPSocketSend.cpp tcp_socket.cpp ws2_32.lib
