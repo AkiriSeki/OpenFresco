83bfbb41fa51ec0652f607d05a500bc2edbedb5e
AkiriSeki <sekia@stanford.edu>
2023-07-13 13:28:34 -0700

    Modified script to create mex depending on OS

--- SRC/simApplicationClient/simulink/create_SFunction.m
+++ SRC/simApplicationClient/simulink/create_SFunction.m
@@ -1,4 +1,13 @@
-mex -O SFun_UDPSocketRecv.c udp_socket.c ws2_32.lib
-mex -O SFun_UDPSocketSend.c udp_socket.c ws2_32.lib
-mex -O SFun_TCPSocketRecv.c tcp_socket.c ws2_32.lib
-mex -O SFun_TCPSocketSend.c tcp_socket.c ws2_32.lib
+% For Windows OS
+if ispc == 1
+    mex -O SFun_UDPSocketRecv.c udp_socket.c ws2_32.lib
+    mex -O SFun_UDPSocketSend.c udp_socket.c ws2_32.lib
+    mex -O SFun_TCPSocketRecv.c tcp_socket.c ws2_32.lib
+    mex -O SFun_TCPSocketSend.c tcp_socket.c ws2_32.lib
+% For Linux OS
+elseif isunix == 1
+    mex -O SFun_UDPSocketRecv.c udp_socket.c
+    mex -O SFun_UDPSocketSend.c udp_socket.c
+    mex -O SFun_TCPSocketRecv.c tcp_socket.c
+    mex -O SFun_TCPSocketSend.c tcp_socket.c
+end
